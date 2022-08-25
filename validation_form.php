278fc7551fe752baf83026ea230212936a41da79
Baptista Jean Baptiste <104376916+TrueCoder2000@users.noreply.github.com>
2022-08-24 20:17:47 -0400

    Test_Project

---
+++ validation_form.php
@@ -0,0 +1,10 @@
+<?php
+
+$btnForm               =    $_POST["btnForm"];
+$name                  =    $_POST["name"];
+$email                 =    $_POST["email"];
+$textArea              =    $_POST["textarea"];
+
+
+
+?>
